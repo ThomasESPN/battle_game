673564527edc8db82e0291f48709d4cfad8edafc
paul-brd02 <paul.bardou@live.fr>
2024-03-21 15:23:58 +0100

    test point de vie

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from Personnage import Personnage
+
+class TestPersonnage(TestCase):
+
+    def test_point_de_vie(self):
+        personnage = Personnage()
+        self.assertEqual(100, personnage.ptv)
+    
